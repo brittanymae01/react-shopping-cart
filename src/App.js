1833b114ea6a6313087377602ab30ddd42523074
brittanymae01 <brittanymae01>
2019-12-09 12:39:29 -0800

    progress

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
+import { ProductContext } from './contexts/ProductContext';
+import { CartContext } from './contexts/CartContext'
+
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
@@ -12,6 +15,7 @@ function App() {
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
+		setCart([...cart, item])
 		// add the given item to the cart
 	};
 
@@ -20,21 +24,27 @@ function App() {
 			<Navigation cart={cart} />
 
 			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
+			<ProductContext.Provider value={{ products, addItem }}>
+				<CartContext.Provider value={cart}>
+					<Route
+						exact
+						path="/"
+						// render={() => (
+						// 	<Products
+						// 		products={products}
+						// 		addItem={addItem}
+						// 	/>
+						// )}
+						component={Products}
 					/>
-				)}
-			/>
 
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
+					<Route
+						path="/cart"
+						component={ShoppingCart}
+					// render={() => <ShoppingCart cart={cart} />}
+					/>
+				</CartContext.Provider>
+			</ProductContext.Provider>
 		</div>
 	);
 }
